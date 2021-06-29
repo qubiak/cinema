a361cba3f8dc2a570e1e7e6f000bc48daddf20b2
qubiak <kubiak1986@gmail.com>
2021-06-29 10:14:57 +0200

    start

---
+++ src/test/java/pl/qubiak/cinema/operating/system/CinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.qubiak.cinema.operating.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
